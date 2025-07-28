4ca5f7c643d3d1c7f0c52c585501234ef6053f7a
k-mull <kmull@cub.uca.edu>
2025-07-28 12:31:25 -0500

    Update Source.cpp

--- Inheritance Practice/Source.cpp
+++ Inheritance Practice/Source.cpp
@@ -3,11 +3,3 @@
 #include <iostream>
 using namespace std;
 
-int main()
-{
-	MotorCycle myMotorcycle;
-	MotorCycle yourMotorcycle("Harley","Sportster",2000,50);
-	cout << myMotorcycle << endl;
-	cout << yourMotorcycle << endl;
-
-}
